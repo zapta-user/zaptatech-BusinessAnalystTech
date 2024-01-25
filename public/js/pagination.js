156c34975b02bee30ffafe83ac001fee83876c3f
Numan Arshad <numan.arshad970@gmail.com>
2024-01-25 19:19:19 +0500

    templating

---
+++ public/js/pagination.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+  const blogCards = $(".blogPost");
+  const itemsPerPage = 6;
+  const container = $(".focusBlog");
+  let currentPage = 1;
+
+  function showPage(pageNumber) {
+      const startIndex = (pageNumber - 1) * itemsPerPage;
+      const endIndex = Math.min(
+          startIndex + itemsPerPage,
+          blogCards.length
+      );
+
+      blogCards.each(function(index) {
+          if (index >= startIndex && index < endIndex) {
+              $(this).css("display", "flex");
+          } else {
+              $(this).css("display", "none");
+          }
+      });
+
+      // Scroll to the top of the container
+      container[0].scrollIntoView({
+          behavior: "smooth"
+      });
+  }
+
+  // Calculate the number of pages based on the number of cards
+  const totalPages = Math.ceil(blogCards.length / itemsPerPage);
+
+  // Create pagination dynamically
+  const pagination = $(".pagination");
+
+  function updatePagination() {
+      pagination.empty();
+
+      // Add First Page link
+      if (currentPage > 1) {
+          pagination.append(
+              $('<li><i class="fas fa-angle-double-left"></i></li>').click(
+                  () => {
+                      currentPage = 1;
+                      showPage(currentPage);
+                      updatePagination();
+                  }
+              )
+          );
+      } else {
+          pagination.append(
+              $('<li><i class="fas fa-angle-double-left"></i></li>').addClass(
+                  "disabled"
+              )
+          );
+      }
+
+      // Add Previous Page link
+      if (currentPage > 1) {
+          pagination.append(
+              $('<li><i class="fas fa-angle-left"></i></li>').click(() => {
+                  currentPage--;
+                  showPage(currentPage);
+                  updatePagination();
+              })
+          );
+      } else {
+          pagination.append(
+              $('<li><i class="fas fa-angle-left"></i></li>').addClass(
+                  "disabled"
+              )
+          );
+      }
+
+      // Add page numbers with ellipsis
+      for (let i = 1; i <= totalPages; i++) {
+          if (
+              i === 1 ||
+              i === totalPages ||
+              (i >= currentPage - 1 && i <= currentPage + 1)
+          ) {
+              const pageItem = $("<li>").text(i);
+              if (i === currentPage) {
+                  pageItem.addClass("activePagination");
+              }
+              pageItem.click(() => {
+                  currentPage = i;
+                  showPage(i);
+                  updatePagination();
+              });
+              pagination.append(pageItem);
+          } else if (pagination.children().last().text() !== "...") {
+              pagination.append($("<li>").text("..."));
+          }
+      }
+
+      // Add Next Page link
+      if (currentPage < totalPages) {
+          pagination.append(
+              $('<li><i class="fas fa-angle-right"></i></li>').click(() => {
+                  currentPage++;
+                  showPage(currentPage);
+                  updatePagination();
+              })
+          );
+      } else {
+          pagination.append(
+              $('<li><i class="fas fa-angle-right"></i></li>').addClass(
+                  "disabled"
+              )
+          );
+      }
+
+      // Add Last Page link
+      if (currentPage < totalPages) {
+          pagination.append(
+              $('<li><i class="fas fa-angle-double-right"></i></li>').click(
+                  () => {
+                      currentPage = totalPages;
+                      showPage(currentPage);
+                      updatePagination();
+                  }
+              )
+          );
+      } else {
+          pagination.append(
+              $(
+                  '<li><i class="fas fa-angle-double-right"></i></li>'
+              ).addClass("disabled")
+          );
+      }
+  }
+
+  // Show the first page by default
+  showPage(1);
+  updatePagination();
+});
---
+++ public/js/script.js
@@ -0,0 +1,172 @@
+var formClass;
+
+$('.moreless-button').click(function() {
+    $('.moretext').slideToggle();
+    if ($('.moreless-button').text() == "Read more") {
+      $(this).text("Show less")
+    } else {
+      $(this).text("")
+    }
+  });
+
+$.validator.addMethod("atLeastOneCheckbox", function (value, element) {
+    return $('input[name="domains"]:checked').length > 0;
+  }, "Please select at least one domain");
+
+$(".becomepartform").validate({
+    rules: {
+        company_name: {
+            required: true,
+        },
+        sponser_name: {
+            required: true,
+        },
+        user_name: {
+            required: true,
+        },
+        email: {
+            required: true,
+            email: true,
+        },
+        domains: {
+            atLeastOneCheckbox: true,
+          },
+    },
+    messages: {
+        company_name: {
+        required: "Enter company name",
+      },
+      sponser_name: {
+        required: "Enter sponser name",
+      },
+      user_name: {
+        required: "Enter user name",
+      },
+      email: {
+        required: "Enter email",
+      },
+      domains: {
+        atLeastOneCheckbox: "Please select at least one domain",
+      },
+    },
+    submitHandler: function (form) {
+      formClass = form;
+      $(form).closest(".modal").modal("hide");
+      $("#succesmodel").modal("show");
+
+    },
+  });
+
+// Contact form footer section
+  $("#myForm").validate({
+    rules: {
+      contactname: {
+        required: true,
+      },
+      contactemail: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      contactname: {
+        required: "Please enter your full name",
+      },
+      contactemail: {
+        required: "Please enter your email address",
+        email: "Please enter a valid email address",
+      },
+    },
+    submitHandler: function (form) {
+      formClass = form;
+      $(form).closest(".modal").modal("hide");
+      $("#succesmodel").modal("show");
+    },
+  });
+//   Contact us form header
+  $("#myForm2").validate({
+    rules: {
+      contactname: {
+        required: true,
+      },
+      contactemail: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      contactname: {
+        required: "Please enter your full name",
+      },
+      contactemail: {
+        required: "Please enter your email address",
+        email: "Please enter a valid email address",
+      },
+    },
+    submitHandler: function (form) {
+      formClass = form;
+      $(form).closest(".modal").modal("hide");
+      $("#succesmodel").modal("show");
+    },
+  });
+
+  $(".submit-form").click(function(){
+    formClass.submit();
+  });
+
+  // bookconsultform form section
+  $("#bookconsultform").validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "Please enter your full name",
+      },
+      email: {
+        required: "Please enter your email address",
+        email: "Please enter a valid email address",
+      },
+    },
+    submitHandler: function (form) {
+      formClass = form;
+      $(form).closest(".modal").modal("hide");
+      $("#succesmodel").modal("show");
+    },
+  });
+
+  // Assuming you have multiple elements with the class "userPhoneNumber"
+  function positionDropdown(element) {
+    var field = $(element);
+    var fieldPos = field.offset();
+    var fieldWidth = field.outerWidth();
+    var fieldHeight = field.outerHeight();
+
+    $(".iti--container").css({
+        position: "absolute",
+        top: fieldPos.top + fieldHeight,
+        left: fieldPos.left,
+        width: fieldWidth,
+        height: 200,
+    });
+}
+
+$("#phoneNumber").on("open:countrydropdown", function() {
+    positionDropdown("#phoneNumber");
+});
+
+$("#contactphoneNum2").on("open:countrydropdown", function() {
+    positionDropdown("#contactphoneNum2");
+});
+
+$("#phnumber").on("open:countrydropdown", function() {
+    positionDropdown("#phnumber");
+});
+
+  
