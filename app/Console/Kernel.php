9a76b64507232afc425c3e895cbff1314ddbae40
Muhammad Usman <55528997+MuhammadUsman678@users.noreply.github.com>
2024-03-14 12:26:35 +0500

    fix queue issues

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('queue:work --tries=3')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --stop-when-empty --tries=3')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path() . '/logs/scheduler.log');
     }
 
     /**
